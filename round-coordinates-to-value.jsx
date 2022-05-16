43e4e4dae74666b21b6adfe30e2a72c8208dcf45
blakeexists <94477118+blakeexists@users.noreply.github.com>
2022-05-16 16:31:59 -0500

    Update and rename Round Point Coordinates.jsx to round-coordinates-to-value.jsx

--- round-coordinates-to-value.jsx
+++ round-coordinates-to-value.jsx
@@ -1,13 +1,9 @@
 /*
-
-
-
-
-
-
-
+Rounds the coordinates of anchors and/or handles to a specified precision
+https://github.com/blakeexists/adobe-illustrator-scripts
 */
 
+// Creates dialog window
 var dialog = new Window("dialog");
   dialog.text = "Round Values";
   dialog.preferredSize.width = 200;
@@ -16,7 +12,7 @@ var dialog = new Window("dialog");
   dialog.spacing = 10;
   dialog.margins = 16;
 
-// Create selection panel
+// Creates selection panel
 var panel1 = dialog.add("panel", undefined, undefined, {name: "panel1"});
   panel1.text = "Apply to";
   panel1.preferredSize.width = 110;
@@ -42,7 +38,7 @@ var checkboxAnchors = panel1.add("checkbox", undefined, undefined, {name: "check
 var checkboxHandles = panel1.add("checkbox", undefined, undefined, {name: "checkboxHandles"});
   checkboxHandles.text = "Handles";
 
-// Create Round to panel
+// Creates precision panel
 var panel2 = dialog.add("panel", undefined, undefined, {name: "panel2"});
   panel2.text = "Round to (" + getUnits() + ")";
   panel2.preferredSize.width = 110;
@@ -55,7 +51,7 @@ var edittextroundTo = panel2.add('edittext {justify: "center", properties: {name
   edittextroundTo.text = "1";
   edittextroundTo.preferredSize.width = 50;
 
-// Create OK and Cancel buttons
+// Creates OK and Cancel buttons
 var groupConfirm = dialog.add("group"),
     buttonOk = groupConfirm.add("button", undefined, "OK"),
     buttonCancel = groupConfirm.add("button", undefined, "Cancel");
@@ -65,77 +61,7 @@ buttonOk.onClick = function(){
   dialog.close();
 };
 
-
-dialog.show();
-
-// =============================================================================
-
-function main() {
-  var doc = app.activeDocument,
-      roundTo = edittextroundTo.text,
-      selPaths = [],
-      selPoints = [];
-
-  getPaths(doc.pageItems, selPaths);
-  getPoints(selPaths, selPoints);
-
-  for (var i = 0, pLen = selPoints.length; i < pLen; i++) {
-    if (checkboxAnchors.value) {
-      selPoints[i].anchor = round(selPoints[i].anchor, roundTo);
-    }
-    if (checkboxHandles.value) {
-      selPoints[i].leftDirection = round(selPoints[i].leftDirection, roundTo);
-      selPoints[i].rightDirection = round(selPoints[i].rightDirection, roundTo);
-    }
-  }
-}
-
-function round(point, roundTo) {
-  return [Math.round(point[0] / roundTo) * roundTo,
-          Math.round(point[1] / roundTo) * roundTo];
-}
-
-// Get paths from selection
-function getPaths(items, arr) {
-  for (var i = 0, iLen = items.length; i < iLen; i++) {
-    var currItem = items[i];
-    try {
-      if (currItem.typename == "GroupItem") {
-        getPaths(currItem.pageItems, arr);
-      }
-      else if (currItem.typename == "PathItem") {
-        arr.push(currItem);
-      }
-      else if (currItem.typename == "CompoundPathItem") {
-        getPaths(currItem.pathItems, arr);
-      }
-      else {
-        currItem.selected = false;
-      }
-    } catch (e){}
-  }
-  return arr;
-}
-
-// Get selected points on paths
-function getPoints(items, arr) {
-  for (var i = 0, iLen = items.length; i < iLen; i++) {
-    if (items[i].pathPoints.length > 1) {
-      var points = items[i].pathPoints;
-      for (var j = 0, pLen = points.length; j < pLen; j++) {
-        if (radiobuttonSel.value == true) {
-          if (isSelected(points[j])) arr.push(points[j]);
-        } else arr.push(points[j]);
-      }
-    }
-  }
-}
-
-// Check if current point is selected
-function isSelected(point) {
-  return point.selected == PathPointSelection.ANCHORPOINT;
-}
-
+// Returns abbreviated document units
 function getUnits(){
   var units = app.activeDocument.rulerUnits.toString().substring(11);
   if (units == "Pixels") return "px";
@@ -150,8 +76,79 @@ function getUnits(){
   else return units;
 }
 
+dialog.show();
+
+// =============================================================================
+
+function main() {
+  var doc = app.activeDocument,
+      roundTo = edittextroundTo.text,
+      selPaths = [],
+      selPoints = [];
+
+  getPaths(doc.pageItems, selPaths);
+  getPoints(selPaths, selPoints);
+
+  for (var i = 0; i < selPoints.length; i++) {
+    if (checkboxAnchors.value) {
+      selPoints[i].anchor = round(selPoints[i].anchor, roundTo);
+    }
+    if (checkboxHandles.value) {
+      selPoints[i].leftDirection = round(selPoints[i].leftDirection, roundTo);
+      selPoints[i].rightDirection = round(selPoints[i].rightDirection, roundTo);
+    }
+  }
+}
+
+// Rounds coordinates to the given precision
+function round(point, roundTo) {
+  return [Math.round(point[0] / roundTo) * roundTo,
+          Math.round(point[1] / roundTo) * roundTo];
+}
+
+// Gets paths from selection
+function getPaths(items, arr) {
+  for (var i = 0; i < items.length; i++) {
+    var currItem = items[i];
+    try {
+      if (currItem.typename == "GroupItem") {
+        getPaths(currItem.pageItems, arr);
+      }
+      else if (currItem.typename == "PathItem") {
+        arr.push(currItem);
+      }
+      else if (currItem.typename == "CompoundPathItem") {
+        getPaths(currItem.pathItems, arr);
+      }
+      else {
+        currItem.selected = false;
+      }
+    } catch (error){}
+  }
+  return arr;
+}
+
+// Gets selected points on paths
+function getPoints(items, arr) {
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].pathPoints.length > 1) {
+      var points = items[i].pathPoints;
+      for (var j = 0; j < points.length; j++) {
+        if (radiobuttonSel.value == true) {
+          if (isSelected(points[j])) arr.push(points[j]);
+        } else arr.push(points[j]);
+      }
+    }
+  }
+}
+
+// Check if current point is selected
+function isSelected(point) {
+  return point.selected == PathPointSelection.ANCHORPOINT;
+}
+
 // =============================================================================
 
 try {
   main();
-} catch(e) {}
+} catch(error) {}
